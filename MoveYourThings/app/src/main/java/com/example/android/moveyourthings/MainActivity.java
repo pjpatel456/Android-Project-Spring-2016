08a532ddae994d8e986e7abe57f88e0d3545b42b
Darssh <darshitsuthar91@gmail.com>
2016-06-05 16:51:23 -0700

    External libraries and Random Id Gernerator code.

--- MoveYourThings/app/src/main/java/com/example/android/moveyourthings/MainActivity.java
+++ MoveYourThings/app/src/main/java/com/example/android/moveyourthings/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android.moveyourthings;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.moveyourthings.Tasks.GetUserId;
+import com.example.android.moveyourthings.Utility.Constant;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContext = MainActivity.this;
+        Constant.random_id = Installation.id(mContext);
+
+        if(Constant.user_id == ""){
+            GetUserId getUserId = new GetUserId(mContext, Constant.GET_USER_ID_URL);
+            getUserId.execute(Constant.random_id);
+        }
+
+        Button userBtn = (Button) findViewById(R.id.moving);
+        userBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Constant.user_id == ""){
+                    GetUserId getUserId = new GetUserId(mContext, Constant.GET_USER_ID_URL);
+                    getUserId.execute(Constant.random_id);
+                }
+                Intent intent = new Intent(mContext, UserPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
---
+++ MoveYourThings/app/src/main/java/com/example/android/moveyourthings/Utility/Constant.java
@@ -0,0 +1,9 @@
+package com.example.android.moveyourthings.Utility;
+
+public class Constant {
+
+    public static String random_id = "";
+    public static String GET_USER_ID_URL = "http://52.40.104.35/application/getUserId?";
+    public static String request_type = "";
+    public static String user_id = "";
+}
