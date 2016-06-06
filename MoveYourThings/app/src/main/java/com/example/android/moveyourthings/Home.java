d2d5e4601bdcedd3411c9f9dd37a296e3f151a77
pjpatel456 <pjpatel456@gmail.com>
2016-06-06 11:25:33 -0700

    Home page layout created

--- MoveYourThings/app/src/main/java/com/example/android/moveyourthings/Home.java
+++ MoveYourThings/app/src/main/java/com/example/android/moveyourthings/Home.java
@@ -1,13 +1,20 @@
 package com.example.android.moveyourthings;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.moveyourthings.Utility.Constant;
 
 
 public class Home extends Fragment{
+
+    Context mContext;
+    public TextView user_id_text;
 
     public Home() {
     }
@@ -16,13 +23,20 @@ public class Home extends Fragment{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home, container, false);
+
+        View view = inflater.inflate(R.layout.home, container, false);
+        mContext = view.getContext();
+        user_id_text = (TextView) view.findViewById(R.id.user_id);
+        System.out.print(user_id_text);
+
+        user_id_text.setText(Constant.user_id);
+        return view;
     }
 
 }
