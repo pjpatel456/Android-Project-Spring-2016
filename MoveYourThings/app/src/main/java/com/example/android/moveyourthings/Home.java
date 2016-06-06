1efb40329e02f107b7e57a279c60d684a60f6a29
parthvi111 <parthvi111@gmail.com>
2016-06-06 14:04:43 -0700

    Video Capture intent check

--- MoveYourThings/app/src/main/java/com/example/android/moveyourthings/Home.java
+++ MoveYourThings/app/src/main/java/com/example/android/moveyourthings/Home.java
@@ -1,11 +1,14 @@
 package com.example.android.moveyourthings;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.android.moveyourthings.Utility.Constant;
@@ -14,6 +17,7 @@ import com.example.android.moveyourthings.Utility.Constant;
 public class Home extends Fragment{
 
     Context mContext;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
     public TextView user_id_text;
 
     public Home() {
@@ -33,9 +37,22 @@ public class Home extends Fragment{
         View view = inflater.inflate(R.layout.home, container, false);
         mContext = view.getContext();
         user_id_text = (TextView) view.findViewById(R.id.user_id);
-        System.out.print(user_id_text);
-
         user_id_text.setText(Constant.user_id);
+
+        Button record = (Button) view.findViewById(R.id.recoding);
+
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(v.getContext().getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                }
+            }
+        });
+
+
         return view;
     }
 
